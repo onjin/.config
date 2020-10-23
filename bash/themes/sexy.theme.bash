ab75b02a9dec5f71f0d992b8e489115d1d379594
Marek Wywiał <onjinx@gmail.com>
2020-10-23 10:51:56 +0200

    feat(misc): cleanup bash variables, add config fzf helpers, and git better diffs

--- bash/themes/sexy.theme.bash
+++ bash/themes/sexy.theme.bash
@@ -20,22 +20,15 @@ if [[ $TERM != dumb ]] && infocmp xterm-256color >/dev/null 2>&1; then export TE
 fi
 
 if tput setaf 1 &> /dev/null; then
-    if [[ $(tput colors) -ge 256 ]] 2>/dev/null; then
-      MAGENTA=$(tput setaf 9)
-      ORANGE=$(tput setaf 172)
-      GREEN=$(tput setaf 190)
-      PURPLE=$(tput setaf 141)
-      WHITE=$(tput setaf 0)
-    else
-      MAGENTA=$(tput setaf 5)
-      ORANGE=$(tput setaf 4)
-      GREEN=$(tput setaf 2)
-      PURPLE=$(tput setaf 1)
-      WHITE=$(tput setaf 7)
-    fi
+    MAGENTA=$(tput setaf 5)
+    ORANGE=$(tput setaf 4)
+    GREEN=$(tput setaf 2)
+    PURPLE=$(tput setaf 1)
+    WHITE=$(tput setaf 7)
     BOLD=$(tput bold)
     RESET=$(tput sgr0)
 else
+        echo "else"
     MAGENTA="\033[1;31m"
     ORANGE="\033[1;33m"
     GREEN="\033[1;32m"
