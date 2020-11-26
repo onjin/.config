c90c8a186cd5c7e6a21fdcb7435aac168be7c80d
Marek Wywiał <onjinx@gmail.com>
2020-11-26 16:16:29 +0100

    feat(i3): add third monitor

--- screenlayout/setup.sh
+++ screenlayout/setup.sh
@@ -4,12 +4,20 @@ SCREENS_COUNT=$(xrandr -q | grep " connected" | wc -l)
 VGA_SCREENS_COUNT=$(xrandr -q | grep " connected" | grep -c "VGA")
 DVI_SCREENS_COUNT=$(xrandr -q | grep " connected" | grep -c "DVI")
 HDMI_SCREENS_COUNT=$(xrandr -q | grep " connected" | grep -c "HDMI")
+DP_SCREENS_COUNT=$(xrandr -q | grep " connected" | grep -c "DP")
 
 DVI_NAME=$(xrandr -q |grep " connected" | grep "DVI"|cut -d\  -f 1)
 HDMI_NAME=$(xrandr -q |grep " connected" | grep "HDMI"|cut -d\  -f 1)
+DP_NAME=$(xrandr -q |grep " connected" | grep "DP"|cut -d\  -f 1)
 
 echo $DVI_NAME
+echo $HDMI_NAME
+echo $DP_NAME
 
-if [ $SCREENS_COUNT = 2 ]; then
-  xrandr --output ${DVI_NAME} --primary --auto --below ${HDMI_NAME}
+if [ ! -z $DP_NAME  ]; then
+  echo xrandr --output ${DVI_NAME} --primary --auto --below ${HDMI_NAME} --output ${DP_NAME} --auto --rotate right --left-of ${HDMI_NAME}
+  xrandr --output ${DVI_NAME} --primary --auto --below ${HDMI_NAME} --output ${DP_NAME} --auto --rotate right --left-of ${HDMI_NAME}
+else
+    echo xrandr --output ${DVI_NAME} --primary --auto --below ${HDMI_NAME}
+    xrandr --output ${DVI_NAME} --primary --auto --below ${HDMI_NAME}
 fi
