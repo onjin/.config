3b0587b0596f90d79fbe03de7ab0326b2cbc1a45
Marek Wywiał <onjinx@gmail.com>
2021-06-18 23:33:36 +0200

    feat(screen):split 1,2,3 screens setup

--- screenlayout/setup.sh
+++ screenlayout/setup.sh
@@ -10,14 +10,18 @@ DVI_NAME=$(xrandr -q |grep " connected" | grep "DVI"|cut -d\  -f 1)
 HDMI_NAME=$(xrandr -q |grep " connected" | grep "HDMI"|cut -d\  -f 1)
 DP_NAME=$(xrandr -q |grep " connected" | grep "DP"|cut -d\  -f 1)
 
-echo $DVI_NAME
-echo $HDMI_NAME
-echo $DP_NAME
+# echo $DVI_NAME
+# echo $HDMI_NAME
+# echo $DP_NAME
 
-if [ ! -z $DP_NAME  ]; then
-  echo xrandr --output ${DVI_NAME} --primary --auto --below ${HDMI_NAME} --output ${DP_NAME} --auto --rotate right --left-of ${HDMI_NAME}
+echo "Found ${SCREENS_COUNT} screens"
+
+if [ $SCREENS_COUNT = 2  ]; then
+  echo "setting 2 screens"
   xrandr --output ${DVI_NAME} --primary --auto --below ${HDMI_NAME} --output ${DP_NAME} --auto --rotate right --left-of ${HDMI_NAME}
+elif [ $SCREENS_COUNT = 3 ]; then
+  echo "setting 3 screens"
+  xrandr --output ${DVI_NAME} --primary --auto --below ${HDMI_NAME}
 else
-    echo xrandr --output ${DVI_NAME} --primary --auto --below ${HDMI_NAME}
-    xrandr --output ${DVI_NAME} --primary --auto --below ${HDMI_NAME}
+  echo "doing nothing"
 fi
